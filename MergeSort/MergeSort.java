46f0c7a397a8e0c8785e7c7c4b056870705149b9
DalamarVelkyn <chlodnickip@gmail.com>
2019-04-15 23:47:55 -0400

    Merge Sort
    Top Down Merge Sort a list of 100 random integers.

---
+++ MergeSort/MergeSort.java
@@ -0,0 +1,92 @@
+import java.util.Random;
+
+class MergeSort
+{
+	void merge(int array[], int l, int m, int r)
+	{
+		int n1 = m - l + 1;
+		int n2 = r - m;
+		
+		int L[] = new int [n1];
+		int R[] = new int [n2];
+		
+		for (int i=0; i<n1; ++i)
+			L[i] = array[l + i];
+		for (int j=0; j<n2; ++j)
+			R[j] = array[m + 1 + j];
+		
+		int i = 0, j = 0;
+		
+		int k = l;
+		
+		while (i < n1 && j < n2)
+		{
+			if (L[i] <= R[j])
+			{
+				array[k] = L[i];
+				i++;
+			}
+			else
+			{
+				array[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while (i < n1)
+		{
+			array[k] = L[i];
+			i++;
+			k++;
+		}
+		
+		while (j < n2)
+		{
+			array[k] = R[j];;
+			j++;
+			k++;
+		}
+	}
+	
+	void sort(int array[], int l, int r)
+	{
+		if (l < r)
+		{
+			int m = (l + r)/2;
+			
+			sort(array, l, m);
+			sort(array, m + 1, r);
+			
+			merge(array, l, m, r);
+		}
+	}
+	
+	static void printArray(int array[])
+	{
+		int n = array.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(i+1 + ". " + array[i] + "\n");
+		System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Random rd = new Random();
+		
+		int array[] = new int[100];
+		for(int i = 0; i < array.length; i++)
+		{
+			array[i] = rd.nextInt();
+		}
+		
+		System.out.println("Given Array");
+		printArray(array);
+		
+		MergeSort ob = new MergeSort();
+		ob.sort(array, 0, array.length-1);
+		
+		System.out.println("\nSorted Array");
+		printArray(array);
+	}
+}
